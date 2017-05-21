9c8ef7801baccb2f7096f89d3d90a8555671e4db
yangyepeng <yangyepeng@gmail.com>
2017-05-22 07:15:16 +0800

    delete useless js. and add column buying_quantity to products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,7 @@ class ProductsController < ApplicationController
   before_action :validate_search_key, only: [:search]
   before_action :authenticate_user!, only: [:add_to_favorite, :quit_favorite]
 
+
   def index
     if params[:category].blank?
       @products = Product.all.order("position ASC")
@@ -25,12 +26,16 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
     if !current_cart.products.include?(@product)
        current_cart.add_product_to_cart(@product)
+       @product.quantity -= @product.buying_quantity
+       @product.buying_quantity = 1
+       @product.save
        flash[:notice] = "你已成功将 #{@product.title} 加入购物车"
     else
       flash[:warning] = "你的购物车内已有此物品"
     end
     redirect_to :back
-  end
+
+end
 
   def add_to_favorite
     @product = Product.find(params[:id])
@@ -43,6 +48,28 @@ class ProductsController < ApplicationController
     @product.users.delete(current_user)
     @product.save
     redirect_to :back, alert: "成功取消收藏!"
+  end
+
+  def add_buying_quantity
+    if @product.buying_quantity <= @product.quantity
+       @product.buying_quantity +=1
+       @product.save
+       # redirect_to :back
+      respond_to do |format|
+        format.js   { render :layout => false }
+      end
+    end
+  end
+
+  def remove_buying_quantity
+    if @product.buying_quantity > 1
+      @product.buying_quantity -= 1
+      @product.save
+      # redirect_to :back
+      respond_to do |format|
+        format.js   { render :layout => false }
+      end
+    end
   end
 
   def search
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170520150304) do
+ActiveRecord::Schema.define(version: 20170521132158) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -75,11 +75,12 @@ ActiveRecord::Schema.define(version: 20170520150304) do
     t.text     "description"
     t.integer  "quantity"
     t.integer  "price"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.string   "image"
     t.integer  "position"
     t.integer  "category_id"
+    t.integer  "buying_quantity", default: 1
   end
 
   create_table "users", force: :cascade do |t|
