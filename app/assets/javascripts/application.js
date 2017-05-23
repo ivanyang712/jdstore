1353d5e94f459fcb77199c8cc2bf3896d1653238
yangyepeng <yangyepeng@gmail.com>
2017-05-24 00:04:08 +0800

    add landing page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,19 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require bootstrap/alert
-//= require bootstrap/dropdown
-//= require chosen-jquery
-//= require jquery.raty
+//= require bootstrap
 //= require_tree .
+
+//轮播
+$(document).ready(function () {
+  $('#myCarousel').carousel({
+    interval: 2000 //目前是2秒播放一张，可以根据需要调整这个值
+  })
+
+
+//商品详情菜单切换
+  $(document).on('click', '.productDecription-menuList-item', function () {
+  var index = $(this).index() //拿到这个「选项卡」的index，第一个是0，第二个是1，以此类推
+  $('.productDecription-content-item').hide() //所有的内容都隐藏
+  $('.productDecription-content-item').eq(index).show() //只显示对于index的内容
+})
